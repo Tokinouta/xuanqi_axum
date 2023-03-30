5f421ad134730bb4de39169d073e2ac48aa5adc7
Hou Boyao <shangrila-1100@hotmail.com>
2023-03-30 18:28:51 +0800

    added a save text method to clipboard

--- src/web_service/clipboard/mod.rs
+++ src/web_service/clipboard/mod.rs
@@ -1,12 +1,23 @@
 use axum::{
+    extract::State,
+    http::StatusCode,
+    response::IntoResponse,
     routing::{get, post},
-    Router,
+    Json, Router,
 };
+use redis::{Client, Commands};
+
+async fn save_text(State(client): State<Client>, Json(req): Json<String>) -> impl IntoResponse {
+    let mut con = client.get_connection().unwrap();
+
+    let key = "name";
+    // let value = req.as_str();
+    con.set::<&str, String, ()>(key, req).unwrap();
+    (StatusCode::OK, "Saved Successfully")
+}
+
+fn save_file() {}
 
 pub fn router() -> Router {
-    let category_router = Router::new();
-    let topic_router = Router::new();
-    Router::new()
-        .nest("/category", category_router)
-        .nest("/topic", topic_router)
+    Router::new().route("/save-txt", post(save_text))
 }
